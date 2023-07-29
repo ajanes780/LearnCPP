d072c11ee0163a073e673a1fdd768637a768209c
Aaron Janes <aaron.janes@gmail.com>
2023-07-29 09:01:21 -0600

    using math package

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+// Calculate the area of a circule
+    float pi = 3.14;
+    float radius = 5.55;
+
+    double area = pi * pow(radius, 2);
+
+    cout  << "the area is equal to " << area;
+
     return 0;
 }
