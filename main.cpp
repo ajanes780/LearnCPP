a53904b985a6d2e56a7ddcf96daf12b33636dc37
Aaron Janes <aaron.janes@gmail.com>
2023-07-29 17:12:18 -0600

    control statements
    Signed-off-by: Aaron Janes <aaron.janes@gmail.com>

--- main.cpp
+++ main.cpp
@@ -5,12 +5,31 @@
 
 using namespace std;
 
-int main() {
-    int x = 30;
-    int y = 3;
+float cal_commission(float rate, float salary) {
+    float comm = salary * rate;
 
-    double z = static_cast<double >(x) / y;
-    cout << z;
-    return 0;
+    return comm;
 }
 
+int main() {
+
+    cout << "What is your salary ? " << endl;
+    float sales;
+    cin >> sales;
+    float commission;
+
+
+    if (sales <= 10'000) {
+        commission = cal_commission(.10, sales);
+        cout << "Your commission is equal to: " << commission << endl;
+
+    }
+    if (sales >= 10'001 && sales <= 15'000) {
+        commission = cal_commission(.15, sales);
+        cout << "Your commission is equal to: " << commission << endl;
+    }
+    if (sales >= 15'001) {
+        commission = cal_commission(.20, sales);
+        cout << "Your commission is equal to: " << commission << endl;
+    }
+}
