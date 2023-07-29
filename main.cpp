6fe21436c6eb5b068ac6e7b448b67eee2f6af0ab
Aaron Janes <aaron.janes@gmail.com>
2023-07-29 10:02:12 -0600

    More math functions

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <cmath>
+#include <ctime>
+
 using namespace std;
 
+
+
 int main() {
-// Calculate the area of a circule
-    float pi = 3.14;
-    float radius = 5.55;
 
-    double area = pi * pow(radius, 2);
+    srand(time(nullptr));
+    short minValue{1};
+    short maxValue{6};
 
-    cout  << "the area is equal to " << area;
+    int dice1 =   (rand() % (minValue - maxValue + 1) + minValue);
+    int dice2 =   (rand() % (minValue - maxValue + 1) + minValue);
+
+    std::cout << "Dice one equals: " << dice1 << endl;
+    std::cout << "Dice two equals: " << dice2 << endl;
+
 
     return 0;
 }
+
