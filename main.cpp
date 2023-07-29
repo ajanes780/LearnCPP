fd01b9d2a495ff53ad5bc1db46fb3e5524118c7d
Aaron Janes <aaron.janes@gmail.com>
2023-07-29 13:23:33 -0600

    creating tables
    Signed-off-by: Aaron Janes <aaron.janes@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <ctime>
+#include <iomanip>
 
 using namespace std;
 
@@ -11,16 +12,22 @@ short rollDice(short a, short b ) {
 
 int main() {
 
-    srand(time(nullptr));
-    short minValue{1};
-    short maxValue{6};
+//    srand(time(nullptr));
+//    short minValue{1};
+//    short maxValue{6};
+//
+//    short dice1 = rollDice(minValue, maxValue);
+//    short dice2 = rollDice(minValue, maxValue);
+//
+//    cout << left;
+//    cout << "Dice one equals: " <<  dice1 << endl;
+//    cout << "Dice two equals: " << dice2 << endl;
+//    cout << fixed << setprecision(4) << 12.345678;
 
-    short dice1 = rollDice(minValue, maxValue);
-    short dice2 = rollDice(minValue, maxValue);
 
-    std::cout << "Dice one equals: " << dice1 << endl;
-    std::cout << "Dice two equals: " << dice2 << endl;
-
+    cout << left << setw(10) << "Course" << setw(10) << "Students" << endl;
+    cout << left << setw(10) << "C++" << right << setw(10) << "100" << endl;
+    cout << left << setw(10) << "JavaScript" << setw(10) << right << "50" << endl;
 
     return 0;
 }
