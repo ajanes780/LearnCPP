f6727d07f7fe4de08e25aa03ac03c26be82a2880
Aaron Janes <aaron.janes@gmail.com>
2023-07-29 13:51:51 -0600

    get line function from std library
    Signed-off-by: Aaron Janes <aaron.janes@gmail.com>

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 
 
-short rollDice(short a, short b ) {
-    return (rand() % (a - b + 1) + a);
-}
+//short rollDice(short a, short b ) {
+//    return (rand() % (a - b + 1) + a);
+//}
 
 int main() {
 
@@ -25,9 +25,28 @@ int main() {
 //    cout << fixed << setprecision(4) << 12.345678;
 
 
-    cout << left << setw(10) << "Course" << setw(10) << "Students" << endl;
-    cout << left << setw(10) << "C++" << right << setw(10) << "100" << endl;
-    cout << left << setw(10) << "JavaScript" << setw(10) << right << "50" << endl;
+//    cout << left << setw(15) << "Course" << setw(10) << "Students" << endl
+//         << left << setw(15) << "C++" << right << setw(10) << "100" << endl
+//         << left << setw(15) << "JavaScript" << setw(10) << right << "50" << endl;
+//
+//    bool isPassing {true};
+//    cout << boolalpha << isPassing << endl;
+
+
+    string street, city, state, zipcode;
+    cout << "What street do you live on? ";
+    getline(cin, street);
+    cout << "What city do you live in ?";
+    getline(cin, city);
+    cout << "what state do you live in ?";
+    getline(cin, state);
+    cout << "What is your zipcode ?";
+    getline(cin, zipcode);
+
+    cout
+            << street << endl
+            << city << ", " << state << ", " << zipcode << endl;
+
 
     return 0;
 }
