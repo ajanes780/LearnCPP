716c8755232c1559c486020178ec24676a7a6760
Aaron Janes <aaron.janes@gmail.com>
2023-07-30 19:42:58 -0600

    bubble sort
    Signed-off-by: Aaron Janes <aaron.janes@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,35 +1,40 @@
+
 #include <iostream>
-#include <cmath>
-#include <ctime>
-#include <iomanip>
 
 using namespace std;
 
-float cal_commission(float rate, float salary) {
-    float comm = salary * rate;
+void swap(int arr[], int x, int j) {
+    int temp;
+    temp = arr[x];
+    arr[x] = arr[j];
+    arr[j] = temp;
 
-    return comm;
 }
+
+void sort(int numbers[], int size) {
+
+    for (int pass = 0; pass < size; pass++) {
+        for (int i = 1; i < size; i++) {
+            if (numbers[i - 1] > numbers[i]) {
+                swap(numbers, i - 1, i);
+            }
+        }
+
+    }
+}
+
 
 int main() {
 
-    cout << "What is your salary ? " << endl;
-    float sales;
-    cin >> sales;
-    float commission;
+    int temp;
+    int arr[] = {20, 34, 12, 45, 821, 9, 3, 10};
+    int i;
 
 
-    if (sales <= 10'000) {
-        commission = cal_commission(.10, sales);
-        cout << "Your commission is equal to: " << commission << endl;
+    sort(arr, size(arr));
 
+    for (int num: arr) {
+        cout << num << endl;
     }
-    if (sales >= 10'001 && sales <= 15'000) {
-        commission = cal_commission(.15, sales);
-        cout << "Your commission is equal to: " << commission << endl;
-    }
-    if (sales >= 15'001) {
-        commission = cal_commission(.20, sales);
-        cout << "Your commission is equal to: " << commission << endl;
-    }
+    return 0;
 }
