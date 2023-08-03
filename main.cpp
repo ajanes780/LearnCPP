edbfa380389f9a42a6e310611a372f2b7dc5cff6
Aaron Janes <aaron.janes@gmail.com>
2023-08-03 07:26:20 -0600

    create a struct
    Signed-off-by: Aaron Janes <aaron.janes@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,29 +3,17 @@
 
 using namespace std;
 
+struct Customer {
+    int id;
+    string name;
+    string email;
+};
 
 int main() {
-
-
-    string name = "Aaron Janes";
-    string name2 = "John Janes";
-
-    cout << boolalpha << name.starts_with("A") << "\n";
-    cout << boolalpha << name.ends_with("s") << "\n";
-    cout << name.back() << "\n";
-    cout << name.front() << "\n";
-
-    cout << name.size() << endl;
-    cout << name.length() << endl;
-    cout << name.insert(0, "I am ") << endl;
-
-    if (name.find("Aa") == 0) {
-        cout << "Not found in this string" << endl;
-    } else {
-        cout << "fount it " << endl;
-    }
-
-
+    Customer customer;
+    customer.id = 22;
+    customer.name = "Aaron";
+    customer.email = "aaron@g.com";
 
 
     return 0;
