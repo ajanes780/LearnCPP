9970880b2cabc373e5a4cca1a2d0d19b0ce0f270
Aaron Janes <aaron.janes@gmail.com>
2023-08-03 07:45:36 -0600

    intro to vectors
    Signed-off-by: Aaron Janes <aaron.janes@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 struct Customer {
-    int id;
+    int id{};
     string name;
     string email;
 };
@@ -14,6 +14,21 @@ int main() {
     customer.id = 22;
     customer.name = "Aaron";
     customer.email = "aaron@g.com";
+
+    Customer cust1 = {33, "roger", "roger@gmail.com"};
+
+//    cout << "cust id " << customer.id << endl;
+
+    vector<Customer> customers;
+    customers.push_back(customer);
+    customers.push_back(cust1);
+    customers.push_back({104, "Frank", "frank@xzt.com"});
+
+    for (const auto &c: customers) {
+        cout << c.name << endl;
+    }
+
+
 
 
     return 0;
