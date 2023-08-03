c5e85048c30f1a07d271251e6c25161ad64aa040
Aaron Janes <aaron.janes@gmail.com>
2023-08-03 07:23:14 -0600

    finish strings
    Signed-off-by: Aaron Janes <aaron.janes@gmail.com>

--- main.cpp
+++ main.cpp
@@ -11,11 +11,21 @@ int main() {
     string name2 = "John Janes";
 
     cout << boolalpha << name.starts_with("A") << "\n";
-    cout << boolalpha << name.ends_with("A") << "\n";
+    cout << boolalpha << name.ends_with("s") << "\n";
     cout << name.back() << "\n";
     cout << name.front() << "\n";
 
     cout << name.size() << endl;
+    cout << name.length() << endl;
+    cout << name.insert(0, "I am ") << endl;
+
+    if (name.find("Aa") == 0) {
+        cout << "Not found in this string" << endl;
+    } else {
+        cout << "fount it " << endl;
+    }
+
+
 
 
     return 0;
