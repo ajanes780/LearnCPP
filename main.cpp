431302f5c1c95865a773cab6162b4a335ee5104b
Aaron Janes <aaron.janes@gmail.com>
2023-08-05 08:16:18 -0600

    beginning oop
    Signed-off-by: Aaron Janes <aaron.janes@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,19 +1,11 @@
-
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
 
-
 int main() {
-    ofstream file;
-    file.open("test-data.txt");
-    if (file.is_open()) {
-        file << "Hello World" << endl;
-        cout << "Finished" << endl;
-        file.close();
-    }
-
+    ifstream file;
+    file.open()
     return 0;
 }
